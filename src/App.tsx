117d07b7e5c03dc7b07c5e29f07a5e6fb68aa512
Gitana Kovanaite <gkovanaite@Gitanas-MacBook-Air.local>
2024-02-02 13:34:52 +0000

    have versions with render content as JSX and function call

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Example from "./components/AddTasks";
+import Example from "./components/AddTasks-fn";
+import Example1 from "./components/AddTasks-jsx";
 import Styled from "./shared/Styled";
 import "./App.css";
 
 function App() {
   return <Styled.Container>
     <Example />
+    <Example1 />
   </Styled.Container>
 }
 
